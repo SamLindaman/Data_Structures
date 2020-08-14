4d3c2024359a37a87c41b0d0873a043a1065e995
Sam Lindaman <samlindaman@gmail.com>
2020-08-13 21:09:18 -0500

    added chaining for colision handeling

--- HashTable.py
+++ HashTable.py
@@ -3,7 +3,7 @@
 class HashTable:
     def __init__(self):
         self.max = 50
-        self.arr = [None for i in range(self.max)]
+        self.arr = [[] for i in range(self.max)]
 
     def getHash(self, key):
         h = 0
@@ -15,12 +15,24 @@ class HashTable:
             self.getHash(self, str(key))
 
     def __setitem__(self, key, value):
+
+        modify = False
         h = self.getHash(key)
-        self.arr[h] = value
+
+        for index, element in enumerate(self.arr[h]):
+            if element[0] == key:
+                self.arr[h][index] = (key, value)
+                modify = True
+
+        if not modify:
+            self.arr[h].append((key, value))
 
     def __getitem__(self, key):
         h = self.getHash(key)
-        return self.arr[h]
+        for element in self.arr[h]:
+            if element[0] == key:
+                return element[1]
+
 
     def set(self, key, value):
         h = self.getHash(key)
@@ -32,20 +44,28 @@ class HashTable:
 
     def __delitem__(self, key):
         h = self.getHash(key)
-        self.arr[h] = None
+        for index, element in enumerate(self.arr[h]):
+            if element[0] == key:
+                del self.arr[h][index]
 
 
 if __name__ == '__main__':
     #using created hash table
     table = HashTable()
-    table['march 15'] = 25
+    table['march 6'] = 25
+    table['march 6'] = 55
+    table['march 17'] = 17
     table['jan 14'] = 100
-    table['abc 22'] = 1
     table.set('August 23', 23)
+
+    print(table.arr)
+
+    del table['march 6']
     del table['abc 22']
 
     print(table.arr)
 
+    print('\r\r')
     #using dictionary
     tableDict = {}
     tableDict['april 14'] = 11
