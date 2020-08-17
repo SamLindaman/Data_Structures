079e3c541dcb8a20cfed71de15cba0f135deebc9
Samuel Lindaman <46086020+SamLindaman@users.noreply.github.com>
2020-08-17 15:17:38 -0500

    Binary Search Tree CPP

--- main.cpp
+++ main.cpp
@@ -1,83 +1,122 @@
 //
 //  main.cpp
-//  HashTable
+//  BST
 //
-//  Created by Sam lindaman on 8/14/20.
+//  Created by Sam lindaman on 8/16/20.
 //  Copyright © 2020 Sam lindaman. All rights reserved.
 //
 
 #include <iostream>
-#include <list>
-#include <string.h>
-
 using namespace std;
+  
+class BST
+{
+    int data;
+    BST *leftNode, *rightNode;
+  
+    public:
+    BST();
+    BST(int);
 
-class HashTable{
-    list<string> *table;
-    int max;
-    
-public:
-    HashTable(int n){
-        max = n;
-        table = new list<string>[max];
-    }
-    
-    int getHashKey(string key){
-        int h = 0;
-        for(int i = 0; i <key.length();i++){
-            h += key[i];
-        }
-        return h % max;
-    }
-    
-    void insert(string key){
-        table[getHashKey(key)].push_back(key);
-    }
-    
-    void remove(string key){
-        int h = getHashKey(key);
-        
-        list<string>::iterator i;
-        for (i=table[h].begin(); i!=table[h].end(); i++) {
-            if(key.compare(i->c_str())){
-                table[h].erase(i);
-            }
-        }
-    }
-    
-    void print(){
-        list<string>::iterator j;
-
-        for(int i =0;i<max;i++){
-            cout<< " Index: "<<i+1<<": ";
-            for(j = table[i].begin();j!=table[i].end();j++){
-                cout<<j->c_str()<<" -> ";
-            }
-            cout<<endl;
-        }
-        cout<<endl;
-    }
-    
+    BST* Insert(BST *, int);
+    void inOrder(BST *);
+    void postOrder(BST *);
+    void preOrder(BST *);
 };
-
-
-int main(int argc, const char * argv[]) {
-    
-    //initialize hash table and fill it with elements
-    HashTable hash(10);
-    
-    string arr[] = {"Sam", "Max", "Ada", "Jill", "Jan"};
-    for(int i =0; i < (sizeof(arr)/sizeof(string)); i++){
-        hash.insert(arr[i]);
+  
+// Default Constructor
+BST :: BST() : data(0), leftNode(NULL), rightNode(NULL){}
+  
+// Constructor with Parameter
+BST :: BST(int value)
+{
+    data = value;
+    leftNode = rightNode = NULL;
+}
+  
+// Insert definition.
+BST* BST :: Insert(BST *root, int value)
+{
+    if(root == NULL)
+    {
+        return new BST(value);
     }
     
-    hash.print();
+    // does not allow for repeat data
+    if (root->data == value){
+        return root;
+    }
     
-    hash.insert("Madi");
+    // Insert data.
+    if(value > root->data)
+    {
+        root->rightNode = Insert(root->rightNode, value);
+    }
+    else
+    {
+        root->leftNode = Insert(root->leftNode, value);
+    }
+      
+    return root;
+}
+  
+// Inorder traversal function.
+void BST :: inOrder(BST *root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+    inOrder(root->leftNode);
+    cout << root->data << " ";
+    inOrder(root->rightNode);
+}
+
+// Post order traversal
+void BST:: postOrder(BST *root){
+    if (root == NULL) {
+        return;
+    }
+    postOrder(root->leftNode);
+    postOrder(root->rightNode);
+    cout<< root->data<<" ";
     
-    hash.remove("Max");
+}
+
+// Pre order traversal
+void BST:: preOrder(BST *root){
+    if (root == NULL) {
+        return;
+    }
+    cout<< root->data<<" ";
+    postOrder(root->leftNode);
+    postOrder(root->rightNode);
     
-    hash.print();
+}
+
+  
+int main()
+{
+    int array[] = {45,67,77,43,1,0,9,33,99,87,45,45,45,47,68,100};
+    
+    BST tree, *root = NULL;
+    
+    root = tree.Insert(root, 55);
+    
+    for(int i = 0;i<*(&array+1)-array;i++){
+        tree.Insert(root, array[i]);
+    }
+  
+    cout<<"In Order: "<<endl;
+    tree.inOrder(root);
+    cout<<endl<<endl;
+    cout<<"Post Order:"<<endl;
+    tree.postOrder(root);
+    cout<<endl<<endl;
+    cout<<"Pre Order:"<<endl;
+    tree.preOrder(root);
+    cout<<endl<<endl;
+    
     
     return 0;
 }
