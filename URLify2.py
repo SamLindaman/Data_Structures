cf62d2980f937453a49c3518c6ae64140232ef17
Samuel Lindaman <46086020+SamLindaman@users.noreply.github.com>
2020-09-16 15:43:37 -0500

    Update URLify2.py

--- URLify2.py
+++ URLify2.py
@@ -16,27 +16,22 @@ def make_new_string(string):
     if not isinstance(string, str):
         make_new_string(str(string))
 
-    startFlag = len(string) - 1
     stopFlag = len(string) - 1
 
     for i in range(len(string) - 1, -1, -1):
         if string[i] == ' ':
-            string = string[:startFlag] + '%20' + string[startFlag + 1:stopFlag] + string[stopFlag:]
-            stopFlag = startFlag + 3
-        startFlag -= 1
+            string = string[:i] + '%20' + string[i + 1:stopFlag] + string[stopFlag:]
+            stopFlag = i + 3
 
     return string
 
 
 if __name__ == '__main__':
     string1 = 'Hello World !'
-    string2 = make_new_string(string1)
-    print(string2 + '\n')
+    print(make_new_string(string1) + '\n')
 
-    string3 = ' this is a test to replace spaces '
-    string4 = make_new_string(string3)
-    print(string4 + '\n')
+    string2 = ' this is a test to replace spaces '
+    print(make_new_string(string2) + '\n')
 
-    string5 = '          '
-    string6 = make_new_string(string5)
-    print(string6)
+    string3 = '          '
+    print(make_new_string(string3))
