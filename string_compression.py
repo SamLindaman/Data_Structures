be26173071863bccd1e0073ee5987d744ae0c0cd
Samuel Lindaman <46086020+SamLindaman@users.noreply.github.com>
2020-09-30 17:46:10 -0500

    cracking the coding interview 1.6

--- string_compression.py
+++ string_compression.py
@@ -1,3 +1,10 @@
+'''
+String Compression: Implement a method to perform basic string compression using the counts of repeated characters.
+For example, the string aabccccaaa would become a2bc5a3. If the compressed string would not becoe smaller than the original
+string, your method should return the length of the original string. You can assume the string has only uppercase and lowercase letters (a-z)
+'''
+
+
 def compress(arr):
     i = 0
     j = 1
